dc73eecab9b0368259d4ead37e6639387b889211
orashar <omparshar15@gmail.com>
2021-09-11 16:16:14 +0530

    proof of work implemented

---
+++ main.js
@@ -0,0 +1,73 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index
+        this.timestamp = timestamp
+        this.data = data
+        this.previousHash = previousHash
+        this.hash = this.calculateHash()
+        this.nonce = 0
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join('0')){
+            this.hash = this.calculateHash()
+            this.nonce++
+        }
+
+        console.log("Block mined : ", this.hash)
+    }
+}
+
+class BlockChain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()]
+        this.difficulty = 4
+    }
+
+    createGenesisBlock(){
+        return new Block(0, '01/01/2021', "Genesis Block", "0")
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length-1]
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash
+        newBlock.mineBlock(this.difficulty)
+        this.chain.push(newBlock)
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i]
+            const previousBlock = this.chain[i-1]
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) return false
+
+            if(currentBlock.previousHash !== previousBlock.hash) return false
+        }
+        return true
+    }
+}
+
+let coin = new BlockChain()
+console.log("mining block 1...")
+coin.addBlock(new Block(1, '02/02,2021', {amount: 4}))
+console.log("mining block 2...")
+coin.addBlock(new Block(2, '12/02,2021', {amount: 10}))
+
+// console.log(coin.isChainValid())
+
+// coin.chain[1].data = { amount : 40}
+// coin.chain[1].hash = coin.chain[1].calculateHash()
+
+// console.log(coin.isChainValid())
+
+console.log(JSON.stringify(coin, null, 4))
